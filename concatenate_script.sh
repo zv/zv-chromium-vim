f6780b7a3d95a4f12ae273db83bc0a81f93b2e35
Zephyr Pellerin <zv@nxvr.org>
2017-05-09 17:40:25 -0700

    Script to concatenate all items

---
+++ concatenate_script.sh
@@ -0,0 +1,24 @@
+declare -a files
+files=(cmdline_frame.js
+    content_scripts/cvimrc_parser.js
+    content_scripts/session.js
+    content_scripts/utils.js
+    content_scripts/dom.js
+    content_scripts/hints.js
+    content_scripts/bookmarks.js
+    content_scripts/keys.js
+    content_scripts/clipboard.js
+    content_scripts/complete.js
+    content_scripts/mappings.js
+    content_scripts/find.js
+    content_scripts/cursor.js
+    content_scripts/status.js
+    content_scripts/hud.js
+    content_scripts/visual.js
+    content_scripts/command.js
+    content_scripts/scroll.js
+    content_scripts/search.js
+    content_scripts/frames.js
+    content_scripts/messenger.js)
+
+cat "${files[@]}" > content_scripts/cvim_all.js
